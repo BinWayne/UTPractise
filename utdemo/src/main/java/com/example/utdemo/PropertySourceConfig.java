f7dfd1f0418ab157ea9f87bb5f4643e3e2a4b2f5
binwayne <36578067@qq.com>
2018-05-25 19:50:08 +0800

    setup property source test

---
+++ utdemo/src/main/java/com/example/utdemo/PropertySourceConfig.java
@@ -0,0 +1,16 @@
+package com.example.utdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@PropertySource("classpath:property-source.properties")
+public class PropertySourceConfig {
+	
+	@Autowired
+    private Environment env;
+	
+	
+}
